4ea9739324f3a318aa7ecbb783d92eee1fab0bc5
Yurev-Sergey <87377996+SergeyDibo@users.noreply.github.com>
2024-02-11 22:07:53 +0300

    start project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Header.module.css";
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <img
+                src='https://e7.pngegg.com/pngimages/510/484/png-clipart-apple-icon-format-computer-icons-apple-white-heart.png'/>
+        </header>
+    )
+}
+
+
+export default Header;
---
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Post.module.css";
+
+const Post = (props) => {
+
+    return (
+        <div className={classes.item}>
+            <img
+                src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png"/>
+            {props.message}
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+}
+export default Post;
